478009c21ba741787a6d11ab6daa5a7ad23d8218
kyle <stcalica@gmail.com>
2015-06-05 23:29:30 -0700

    changed ui for making request

--- app/src/main/java/teaminfamous/com/friendsend/FBLogin.java
+++ app/src/main/java/teaminfamous/com/friendsend/FBLogin.java
@@ -61,7 +61,7 @@ public class FBLogin  extends ActionBarActivity{
     JSONObject fbResponse;
     String fb_user_id;
     String fb_name;
-    String sqlurl = "jdbc:postgresql://10.0.2.2/FriendSend?user=postgres&password=Batman4738473";
+    String sqlurl = "jdbc:postgresql://10.0.2.2/FriendSend?user=postgres&password=barry1";
 //    String url = "jdbc:postgresql://10.0.2.2/test?user=postgres&password=barry1";
 
     public void PopulateFriends() {
