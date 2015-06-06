a622c409333c8b87fcbb61de9effbbb5898a840f
JBrown903 <jmbrownlee@ucdavis.edu>
2015-06-05 23:37:17 -0700

    Added AddPackageQuery

--- app/src/main/java/teaminfamous/com/friendsend/makeRequestDetails.java
+++ app/src/main/java/teaminfamous/com/friendsend/makeRequestDetails.java
@@ -1,11 +1,28 @@
 package teaminfamous.com.friendsend;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class makeRequestDetails extends ActionBarActivity {
+
+    private int package_id; // the package id
+    private String package_name; // the name of the package to be sent
+    private int sender_id; // the user_id of the package sender
+    private int receiver_id; // the user_id of the package receiver
+    private String date_for_delivery; // the date for the package to be delivered.
+    private int package_trust_level; // the level of trust for the package
+    private String package_description; // the package description
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +51,55 @@ public class makeRequestDetails extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-}
+    } // onOptionsItemSelected(MenuItem item)
+
+    public class AddPackageQuery extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String retr = "";
+            try{
+                Class.forName("org.postgresql.Driver");
+
+            } catch (ClassNotFoundException e){
+
+                e.printStackTrace();
+                retr = e.toString();
+            }
+
+            Connection conn;
+            try{
+                Log.d("JakeDebug", "AddPackageQuery: ");
+                DriverManager.setLoginTimeout(15);
+                conn = DriverManager.getConnection(sqlurl);
+                Statement st = conn.createStatement();
+                Log.d("JakeDebug", "AddUserQuery: just before query");
+                String query = "INSERT INTO _parcels_ (   ) VALUES(" + package_id + ", '" + package_name + "');"; //actual query
+                ResultSet rs = st.executeQuery(query);
+                Log.d("JakeDebug", "AddUserQuery: just after query");
+                Boolean empty = true;
+                while(rs.next()){
+                    Log.d("JakeDebug", "Inside while: " + rs.getString("name"));
+                    retr = rs.getString("name"); //column data wanted or amount
+                    empty = false;
+                }
+                //no matches
+                if(empty){
+
+                    retr = null;
+                }
+
+                rs.close();
+                st.close();
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                retr = e.toString();
+            }
+            return retr;
+
+        }
+    }//end of add user
+
+} // class makeRequestDetails
