765ecdbcd952f815e4be1589179b2b0f398da269
kyle <stcalica@gmail.com>
2015-06-08 14:44:48 -0700

    adding commits for what must be done

--- app/src/main/java/teaminfamous/com/friendsend/parcels.java
+++ app/src/main/java/teaminfamous/com/friendsend/parcels.java
@@ -4,8 +4,13 @@ package teaminfamous.com.friendsend;
  * Created by kyle on 6/8/2015.
  */
 public class parcels {
-
+    //MUST match database if add in here then add to database!
     public String name;
-    public double latitude;
-    public double longitude;
+    public double latitude; //change to origin latitude
+    public double longitude;//change to origin longitude
+    //include destination coordinates
+    //include user id to track sender
+    //user id to include reciever
+    //user id to include current transmitter
+
 }
