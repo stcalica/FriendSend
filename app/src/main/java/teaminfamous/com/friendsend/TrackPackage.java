f3f58e01e6d586c6ba132f5148032d7f20eb6467
kyle <stcalica@gmail.com>
2015-06-08 10:14:14 -0700

    track location done

--- app/src/main/java/teaminfamous/com/friendsend/TrackPackage.java
+++ app/src/main/java/teaminfamous/com/friendsend/TrackPackage.java
@@ -6,9 +6,12 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.sql.Connection;
@@ -16,6 +19,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class TrackPackage extends FragmentActivity {
     String sqlurl = "jdbc:postgresql://10.0.2.2/FriendSend?user=postgres&password=barry1";
@@ -23,11 +27,14 @@ public class TrackPackage extends FragmentActivity {
     public Location markers[];
     int uid = 1; //once we get it ignore it
 
+    ArrayList<parcels> mPoints = new ArrayList<parcels> ();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_package);
         setUpMapIfNeeded();
+
         //get user id
         //all packages from datatbase
         new GetPackagesQuery().execute();
@@ -76,7 +83,10 @@ public class TrackPackage extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+         // CameraUpdate move = CameraUpdateFactory.newLatLng(new LatLng(121.7381, -38.5539)); wrong order!
+        CameraUpdate zoom =  CameraUpdateFactory.newLatLngZoom(new LatLng(38.5539, -121.7381), 13);
+        mMap.moveCamera(zoom);
+
     }
 
     public class GetPackagesQuery extends AsyncTask<Void, Void, String> {
@@ -96,17 +106,21 @@ public class TrackPackage extends FragmentActivity {
                 conn = DriverManager.getConnection(sqlurl);
                 Statement st = conn.createStatement();
                 Log.d("JakeDebug", "AddPackageQuery: just before query");
-                //String query = "INSERT INTO _parcels_ (name, sender, deliv_date, trust_level, description) VALUES('Drugs', 10001, '9/11/2001', 666, 'Not suspicious');";
+                //String query = "SELECT name, long, lat FROM _parcels_ where id = "+  +";";
                 String query = "SELECT name, long, lat FROM _parcels_ where id=" + uid; //actual query
                 Log.d("JakeDebug", "AddPackageQuery: query = \"" + query + "\"");
                 ResultSet rs = st.executeQuery(query);
-                Boolean empty = true;
                 while(rs.next()){
-                    empty = false;
-                }
-                //no matches
-                if(empty){
-                    Log.d("JakeDebug", "Login Query: empty = true");
+                    double longitude =  rs.getDouble("long");
+                    double latitude = rs.getDouble("lat");
+                    String name = rs.getString("name");
+                    parcels temp = new parcels();
+                    temp.name = name;
+                    temp.longitude = longitude;
+                    temp.latitude = latitude;
+                    mPoints.add(temp);
+                  //  mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(name));
+
                 }
                 Log.d("JakeDebug", " just after query");
                 st.close();
@@ -115,6 +129,33 @@ public class TrackPackage extends FragmentActivity {
                 e.printStackTrace();
             }
             return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(mPoints != null){
+                //set points
+                if(mMap == null){
+                    Log.d("JakeDebug", "map is null");
+
+
+                }
+                for(int i=0; i < mPoints.size(); i++){
+                    parcels tmp = mPoints.get(i);
+                    Log.d("JakeDebug", "marker at " + tmp.longitude +","+ tmp.latitude);
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(tmp.longitude, -(tmp.latitude))).title(tmp.name).visible(true));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(tmp.longitude, -(tmp.latitude)), 13));
+                    Log.d("JakeDebug", "Adding marker");
+
+                }
+
+            } else{
+                Log.d("JakeDebug", " mPoints is null");
+
+
+            }
 
         }
     }//end of location query
---
+++ app/src/main/java/teaminfamous/com/friendsend/parcels.java
@@ -0,0 +1,11 @@
+package teaminfamous.com.friendsend;
+
+/**
+ * Created by kyle on 6/8/2015.
+ */
+public class parcels {
+
+    public String name;
+    public double latitude;
+    public double longitude;
+}
